123e65794010167cdef16d807f1a08026ae10447
aqifnaimm <129200167+aqifnaimm@users.noreply.github.com>
2026-08-11 21:36:36 +0800

    Redesign homepage navy blue

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -194,7 +194,7 @@ export default function AdminPage() {
             </div>
 
             <p className="admin-motto">
-              BERSATU <b>•</b> BERSETIA <b>•</b> BERKHIDMAT
+              BERSATU <b>•</b> BERKHIDMAT <b>•</b> BERKORBAN
             </p>
           </div>
         </section>
