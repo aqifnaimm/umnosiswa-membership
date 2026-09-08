60c50837fbd7ffa0ec6dbba7eac2e1ba5c61feae
aqifnaimm <129200167+aqifnaimm@users.noreply.github.com>
2026-09-08 20:42:42 +0800

    stay

--- app/daftar/page.tsx
+++ app/daftar/page.tsx
@@ -33,7 +33,6 @@ const ipts = [
   { name: "UMT", zone: "Pantai Timur" },
   { name: "UMS", zone: "Sabah" },
   { name: "UIAM", zone: "Lembah Klang" }
-  { name: "UTM", zone: "Selatan" }
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 const months = [
